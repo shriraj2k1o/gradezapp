208bd6947e685fcab5fcd60ac7ddaab4a89cce7e
Shriraj Jadhav <63845849+shriraj2k1o@users.noreply.github.com>
2020-09-12 01:31:52 +0530

    removed previous login validation

--- sitehome/views.py
+++ sitehome/views.py
@@ -12,15 +12,5 @@ def getStarted(request):
 
 
 def getDemo(request):
-    if request.method=='GET':
-        email = request.GET['mail']
-        password = request.GET['pass']
-
-        user = Users.objects.filter(email=mail)
-
-        if user:
-            redirect(request, 'student/index.html')
-        else:
-            err = "Account doesn't Exist"
-        print('Dataposted -->' + mail + password)
-    return render(request, 'sitehome/login.html', { 'err' : err})
+    
+    return render(request, 'sitehome/login.html')
