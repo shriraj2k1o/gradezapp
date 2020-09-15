2c54eb3737b99e31b8e5dbc3b39686ad9413ba80
Shriraj Jadhav <63845849+shriraj2k1o@users.noreply.github.com>
2020-09-16 00:00:04 +0530

    created EXAM app, completed all dashboard GUI, all minute details as login name, email, profession

--- siteadmin/views.py
+++ siteadmin/views.py
@@ -3,8 +3,15 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def AdminDashboard(request):
     email = request.session.get('email')
+    name = request.session.get('name')
+    profession = request.session.get('profession')
     if email:
-        return render(request, 'siteadmin/index.html')
+        if profession == 'A':
+            return render(request, 'siteadmin/index.html', {'name':name})
+        elif profession == 'S':
+            return redirect('student:index')
+        elif profession == 'P':
+            return redirect('professor:index')
     else:
        
         return redirect('sitehome:login')
--- student/views.py
+++ student/views.py
@@ -3,8 +3,15 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def StudentDashboard(request):
     email = request.session.get('email')
+    name = request.session.get('name')
+    profession = request.session.get('profession')
     if email:
-        return render(request, 'student/index.html')
+        if profession == 'S':
+            return render(request, 'student/index.html', {'name':name})
+        elif profession == 'P':
+            return redirect('professor:index')
+        elif profession == 'A':
+            return redirect('siteadmin:index')
     else:
        
         return redirect('sitehome:login')
